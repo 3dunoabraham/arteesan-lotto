a3fb11f8379f250f1c8c6132cc65b6cb0ce0bf11
arteesanio <gamearteesan@gmail.com>
2022-06-10 20:58:18 -0400

    fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Arteesan 
-Open Source Web3 DApp Builder (built on Vue)
+Open Source Web3 DApp Builder (built with Vue)
 
 ## Try it out
 * npm install
--- src/modules/home-page.vue
+++ src/modules/home-page.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
             <div class="flex-1 tx-center letter-s-3 opacity-50">
-                <h1>Web3 <br> {{LANG.appDesc}} <br> (w / Vue)</h1>
+                <h1>Web3 <br> {{LANG.appDesc}} <br> (Vue)</h1>
             </div>
         <div class="flex-column flex-lg2x-row">
 
